c6c1499ce52e49bb6afde6443e2e18e7ae6a24c7
Ethan Hocking <eshocking253@gmail.com>
2020-05-28 14:45:05 -0600

    Update spec

--- spec/views/partials/_navbar.html.erb_spec.rb
+++ spec/views/partials/_navbar.html.erb_spec.rb
@@ -1,27 +1,30 @@
 require "rails_helper"
 
 describe "partials/_navbar" do
+  let(:user) { User.first }
+
   context "as a visitor" do
     it "I can see a navbar at the top of the page" do
-      render
+      visit "/"
 
-      expect(rendered).to have_link "Find Gear"
-      expect(rendered).to have_link "Login"
-      expect(rendered).to have_link "Register"
-      # TODO: need to test that the actual links have the right hrefs
+      expect(rendered).to have_link "Find Gear", href: "/"
+      expect(rendered).to have_link "Login", href: "/login"
+      expect(rendered).to have_link "Register", href: "/register"
     end
   end
 
   context "as a logged in user" do
-    it "I can see a navbar at the top of the page" do
-      # TODO: set the current user here
+    before do
+      # mock current_user
+      allow(view).to receive(:current_user).and_return(user)
+    end
 
+    xit "I can see a navbar at the top of the page" do
       render
 
-      expect(rendered).to have_link "Find Gear"
-      expect(rendered).to have_link "Log Out"
-      expect(rendered).to have_link "Profile"
-      # TODO: need to test that the actual links have the right hrefs
+      expect(rendered).to have_link "Find Gear", href: "/"
+      expect(rendered).to have_link "Log Out", href: "/logout"
+      expect(rendered).to have_link "Profile", href: "/profile"
     end
   end
 end
