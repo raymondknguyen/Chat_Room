dde48043f835ac944450f914961b2a9715aecb81
Margo Flewelling <flewelling.margo@gmail.com>
2020-05-28 17:22:59 -0600

    Add user profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/profile', to: 'users#show'
+  get '/profile/edit', to: 'users#edit'
+
+  namespace :user do
+    get '/messages', to: "messages#index"
+  end
+
 end
