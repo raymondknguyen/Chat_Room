ae39c8b3d5f99a6f5fb6b9274e2eda1006c46a19
Margo Flewelling <flewelling.margo@gmail.com>
2020-05-28 17:23:19 -0600

    Delete edit profile route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/profile', to: 'users#show'
-  get '/profile/edit', to: 'users#edit'
 
   namespace :user do
     get '/messages', to: "messages#index"
