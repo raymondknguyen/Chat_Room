9379c2ab350e39b7ef12ba8cae5c10514bdce28b
Ethan Hocking <eshocking253@gmail.com>
2020-05-29 11:12:47 -0600

    Add test user

--- spec/views/partials/_navbar.html.erb_spec.rb
+++ spec/views/partials/_navbar.html.erb_spec.rb
@@ -1,7 +1,18 @@
 require "rails_helper"
 
 describe "partials/_navbar" do
-  let(:user) { User.first }
+  let(:user) { User.create!( username: "Kayaking Kate",
+                         name: "Kate Smith",
+                         email: "kate@example.com",
+                         password: "password",
+                         street_address: "123 Mulberry Street",
+                         state: "CO",
+                         zip: "80303",
+                         profile_picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDNCFOXFv30vvRl4plrMuwCltmT5m4TzAtszYfm8XrLP7PLgKH&usqp=CAU",
+                         phone_number: "(123) 456-7891",
+                         age: "25",
+                         about_me: "I love travelling and going on adventures with my favorite side-kick -- my dog, Jack."
+                       )}
 
   context "as a visitor" do
     before do
@@ -9,7 +20,7 @@ describe "partials/_navbar" do
     end
 
     it "I can see a navbar at the top of the page" do
-      render
+      render partial: "partials/navbar.html.erb"
 
       expect(rendered).to have_link "Find Gear", href: "/"
       expect(rendered).to have_link "Login", href: "/login"
@@ -23,7 +34,7 @@ describe "partials/_navbar" do
     end
 
     it "I can see a navbar at the top of the page" do
-      render
+      render partial: "partials/navbar.html.erb"
 
       expect(rendered).to have_link "Find Gear", href: "/"
       expect(rendered).to have_link "Log Out", href: "/logout"
