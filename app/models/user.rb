d29adb4cc818addbe0931ba8711d54a115e502ad
Margo Flewelling <flewelling.margo@gmail.com>
2020-05-29 22:50:57 -0600

    Add user creation from omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,21 @@ class User < ApplicationRecord
   has_many :gear_items, through: :rentals
 
   validates :name, presence: true
-  validates :username, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true
-  validates :password_digest, presence: true
-  validates :street_address, presence: true
-  validates :state, presence: true
-  validates :zip, presence: true
-  validates :phone_number, presence: true
-  validates :age, numericality: true, presence: true
+  # validates :username, uniqueness: true, presence: true
+  # validates :email, uniqueness: true, presence: true
+  # validates :password_digest, presence: true
+  # validates :street_address, presence: true
+  # validates :state, presence: true
+  # validates :zip, presence: true
+  # validates :phone_number, presence: true
+  # validates :age, numericality: true, presence: true
 
-  has_secure_password
+def self.from_omniauth(auth)
+  where(email: auth.info.email).first_or_initialize do |user|
+    user.name = auth.info.name
+    user.email = auth.info.email
+  end
+end
 
   def current_rentals
     rentals.where("end_date >= ? AND start_date <= ?", Date.today, Date.today)
