64c2027e520a14ae353413e8218caff1ffe349cd
Margo Flewelling <flewelling.margo@gmail.com>
2020-05-29 19:52:36 -0600

    Add status to rentals with default of pending

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_024745) do
+ActiveRecord::Schema.define(version: 2020_05_30_014932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_024745) do
     t.bigint "gear_item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "pending"
     t.index ["gear_item_id"], name: "index_rentals_on_gear_item_id"
     t.index ["user_id"], name: "index_rentals_on_user_id"
   end
