25fb3d1f595071dfd4e21f2f196d77b9d363c844
Ethan Hocking <eshocking253@gmail.com>
2020-05-30 14:49:37 -0600

    Add stubbed user to item show page

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -18,6 +18,19 @@ RSpec.describe 'as a user' do
     end
 
     it 'can has item' do
+      user = User.create!( username: "Kayaking Kate",
+                            name: "Kate Smith",
+                            email: "kate@example.com",
+                            street_address: "123 Mulberry Street",
+                            state: "CO",
+                            zip: "80303",
+                            profile_picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDNCFOXFv30vvRl4plrMuwCltmT5m4TzAtszYfm8XrLP7PLgKH&usqp=CAU",
+                            phone_number: "(123) 456-7891",
+                            age: "25",
+                            about_me: "I love travelling and going on adventures with my favorite side-kick -- my dog, Jack."
+                          )
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      
       owner = Owner.create(name: 'owner')
       item1 = create(:random_item, owner: owner)
 
