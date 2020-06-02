39f475c0b1e17e368d213aadae31d62e2a2fb4a3
“Krista <stadler.krista@gmail..com>
2020-06-02 15:13:42 -0600

    array is working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,6 +6,37 @@ class SearchController<ApplicationController
   def index
     @gear_items = GearItem.find_matches(query_params[:keyword].downcase)
     #I think this is where we'll want to make the call to our microservice, using @gear_items and location_params
+    @coordinates = {"latitude"=>39.750605, "longitude"=>-104.990926}
+    parsed_response = [{"id"=>"1137",
+      "type"=>"gear_item",
+      "attributes"=>
+       {"id"=>"1137",
+        "name"=>"Helmet",
+        "location"=>"Denver, CO",
+        "user_location"=>"Denver, CO",
+        "distance"=>"15",
+        "coordinates"=>{"latitude"=>39.750605, "longitude"=>-104.990926}}},
+     {"id"=>"1139",
+      "type"=>"gear_item",
+      "attributes"=>
+       {"id"=>"1139",
+        "name"=>"Purple Helmet",
+        "location"=>"Denver, CO",
+        "user_location"=>"Denver, CO",
+        "distance"=>"15",
+        "coordinates"=>{"latitude"=>39.750605, "longitude"=>-104.990926}}}]
+
+      results = []
+      parsed_response.each do |item|
+        result = {lat: item["attributes"]["coordinates"]["latitude"],
+         lng: item["attributes"]["coordinates"]["longitude"],
+         name: item["attributes"]["name"],
+         url: "/gear_items/#{item["attributes"]["id"]}"
+        }
+        results << result
+      end
+      @json_results = results.to_json.html_safe
+
   end
 
   private
