d52e8eccb9bd4d8ba1ca99e4e79fdf6c968800ca
Raymond Nguyen <rayknguyen@gmail.com>
2020-06-02 17:26:37 -0700

    Delete method

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -13,10 +13,4 @@ class MessageBroadcastJob < ApplicationJob
 
     ActionCable.server.broadcast "room_channel_#{message.room_id}", mine: mine, theirs: theirs, message: message
   end
-
-  private
-
-  def render_message(message)
-    ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message })
-  end
 end
