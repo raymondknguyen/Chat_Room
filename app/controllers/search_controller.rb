d694b4325ae29ec465aef2809e35caace68f7aae
“Krista <stadler.krista@gmail..com>
2020-06-03 16:42:44 -0600

    Refactored API call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,29 +5,30 @@ class SearchController<ApplicationController
 
   def index
     gear_items = GearItem.find_matches(query_params[:keyword].downcase)
-    #I think this is where we'll want to make the call to our microservice, using @gear_items and location_params
     request = GearItemSerializer.new(gear_items, {params: {location: location_params[:location],
                                                                      distance: location_params[:distance]}}).serialized_json
 
-    conn = Faraday.new(
-      url: 'https://frozen-coast-45441.herokuapp.com')
 
-    item_results = conn.post("/locations?items=#{request}")
-    coordinate_results = conn.post("/user_location?items=#{request}")
+    # conn = Faraday.new(
+    #   url: 'https://frozen-coast-45441.herokuapp.com')
+    #
+    # item_results = conn.post("/locations?items=#{request}")
+    # coordinate_results = conn.post("/user_location?items=#{request}")
+    item_response = GearHubService.new.item_results(request)
+    @parsed_item_response = JSON.parse(item_response.body)
+    results = []
+    @parsed_item_response.each do |item|
+      result = {lat: item["attributes"]["coordinates"]["latitude"],
+       lng: item["attributes"]["coordinates"]["longitude"],
+       name: item["attributes"]["name"],
+       url: "/gear_items/#{item["attributes"]["id"]}"
+      }
+      results << result
+    end
+    @json_results = results.to_json.html_safe
 
-      @parsed_response = JSON.parse(item_results.body)
-
-      results = []
-      @parsed_response.each do |item|
-        result = {lat: item["attributes"]["coordinates"]["latitude"],
-         lng: item["attributes"]["coordinates"]["longitude"],
-         name: item["attributes"]["name"],
-         url: "/gear_items/#{item["attributes"]["id"]}"
-        }
-        results << result
-      end
-      @json_results = results.to_json.html_safe
-      @coordinates = JSON.parse(coordinate_results.body)
+    coordinate_response = GearHubService.new.coordinate_results(request)
+    @coordinates = JSON.parse(coordinate_response.body)
   end
 
   private
