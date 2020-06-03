afb8c22367bb0300cee8bf4bbb2acc4c573172fb
Raymond Nguyen <rayknguyen@gmail.com>
2020-06-02 17:19:53 -0700

    Fix button to send

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,14 +1,33 @@
-App.room = App.cable.subscriptions.create({
-  channel: "RoomChannel",
-  room_id: 3
-}, {
-  connected: function () {
-    console.log('connected...');
-  },
-  disconnected: function () {
-    console.log('disconnected...');
-  },
-  received: function (data) {
-    return console.log(data);
-  }
-});
+document.addEventListener('turbolinks:load', () => {
+  const room_element = document.getElementById('room-id');
+  const room_id = Number(room_element.getAttribute('data-room-id'));
+
+  App.room = App.cable.subscriptions.create({
+    channel: "RoomChannel",
+    room_id: room_id
+  }, {
+    connected: function () {
+      console.log('connected to ' + room_id);
+    },
+    disconnected: function () {
+      console.log('disconnected...');
+    },
+    received: function (data) {
+      const user_element = document.getElementById('user-id');
+      const user_id = Number(user_element.getAttribute('data-user-id'));
+  
+      let html;
+  
+      if (user_id === data.message.user_id) {
+        html = data.mine 
+      } else {
+        html = data.theirs
+    
+      }
+  
+      const messageContainer = document.getElementById('messages')
+      messageContainer.innerHTML = messageContainer.innerHTML + html
+    }
+  });
+})
+
--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,7 +2,16 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast 'room_channel', message: render_message(message)
+    mine = ApplicationController.render(
+      partial: 'messages/mine', 
+      locals: { message: message }
+    )
+    theirs = ApplicationController.render(
+      partial: 'messages/theirs', 
+      locals: { message: message }
+    )
+
+    ActionCable.server.broadcast "room_channel_#{message.room_id}", mine: mine, theirs: theirs, message: message
   end
 
   private
