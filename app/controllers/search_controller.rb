9d2404f4fcd9156d50b2b8cc566dcb844f36af2d
“Krista <stadler.krista@gmail..com>
2020-06-03 17:05:01 -0600

    Finalized search functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,32 +6,27 @@ class SearchController<ApplicationController
   def index
     gear_items = GearItem.find_matches(query_params[:keyword].downcase)
     request = GearItemSerializer.new(gear_items, {params: {location: location_params[:location],
-                                                                     distance: location_params[:distance]}}).serialized_json
+                                                           distance: location_params[:distance]}}).serialized_json
+    get_matching_items(request)
+    get_user_coordinates(request)
+  end
 
+  private
 
-    # conn = Faraday.new(
-    #   url: 'https://frozen-coast-45441.herokuapp.com')
-    #
-    # item_results = conn.post("/locations?items=#{request}")
-    # coordinate_results = conn.post("/user_location?items=#{request}")
+  def get_matching_items(request)
     item_response = GearHubService.new.item_results(request)
-    @parsed_item_response = JSON.parse(item_response.body)
-    results = []
-    @parsed_item_response.each do |item|
-      result = {lat: item["attributes"]["coordinates"]["latitude"],
-       lng: item["attributes"]["coordinates"]["longitude"],
-       name: item["attributes"]["name"],
-       url: "/gear_items/#{item["attributes"]["id"]}"
-      }
-      results << result
+    parsed_item_response = JSON.parse(item_response.body)
+    @results = parsed_item_response.map do |item_info|
+      MatchingItem.new(item_info)
     end
-    @json_results = results.to_json.html_safe
+    @json_results = @results.to_json.html_safe
+  end
 
+  def get_user_coordinates(request)
     coordinate_response = GearHubService.new.coordinate_results(request)
     @coordinates = JSON.parse(coordinate_response.body)
   end
 
-  private
 
   def query_params
     params.permit(:keyword)
