8ccf5e3ab0e3f61e50975340b14408e773ce3f8b
“Krista <stadler.krista@gmail..com>
2020-06-04 09:55:47 -0600

    Refactored item to show price and photo

--- app/models/matching_item.rb
+++ app/models/matching_item.rb
@@ -4,14 +4,20 @@ class MatchingItem
               :name,
               :url,
               :id,
-              :location
+              :location,
+              :price,
+              :photo
+
 
   def initialize(attributes)
+    item = GearItem.find(attributes["attributes"]["id"])
     @lat = attributes["attributes"]["coordinates"]["latitude"]
     @lng = attributes["attributes"]["coordinates"]["longitude"]
     @name = attributes["attributes"]["name"]
     @id = attributes["attributes"]["id"]
     @url = "/gear_items/#{@id}"
     @location = attributes["attributes"]["location"]
+    @price = item.price
+    @photo = item.photo
   end
 end
